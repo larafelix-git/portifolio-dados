f8588bdfcd22dc62a3b197a4ea0a540089e04e69
Lara Félix <114635155+larafelix-git@users.noreply.github.com>
2025-05-29 18:36:01 -0300

    Update db.py

--- sistema-medico-agendamento/db.py
+++ sistema-medico-agendamento/db.py
@@ -4,6 +4,6 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="08101991",
+        password="***********",
         database="sistema_medico_agendamentos"
     )
